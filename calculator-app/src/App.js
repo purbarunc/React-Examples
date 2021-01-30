22db76b7ee6e734a598818af5c1f87e881d7e1b4
Purbarun <purbarun@gmail.com>
2021-01-30 23:17:33 +0530

    Initial Commit

---
+++ calculator-app/src/App.js
@@ -0,0 +1,11 @@
+import CalculatorComponent from './components/CalculatorComponent';
+
+function App() {
+  return (
+    <div>
+      <CalculatorComponent/>
+    </div>
+  );
+}
+
+export default App;
